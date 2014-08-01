aa4be32b92a3e502ce500870bfb4312c5d784663
Bryan Taylor <bryan.taylor@sabre.com>
2014-08-01 07:43:33 -0500

    add vcap? boolean

---
+++ CHANGES.md
@@ -0,0 +1,3 @@
+0.0.2 added Cfenv.vcap? boolean to answer if we are running in CloudFoundry environment
+
+0.0.1 initital version
--- lib/cfenv.rb
+++ lib/cfenv.rb
@@ -8,4 +8,10 @@ require 'cfenv/application'
 module Cfenv
   extend Cfenv::Services
   extend Cfenv::Application
+
+  def self.vcap?
+    !vcap_application.empty?
+  end
+
+
 end
